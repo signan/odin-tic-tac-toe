c0cef9853a2ba438e75c7ca1648a7a3270f09abc
signan <signmeup132@gmail.com>
2022-04-21 23:00:09 +0300

    renderGameboard function which removes the cells first, then draws them individually

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 const Gameboard = (() => {
-    let gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let gameboard = [0, 'X', 'X', 3, 4, 'O', 'O', 'X', 8]
 
-    return {gameboard};
+    const getGameboard = () => gameboard;
+    return {getGameboard};
 })();
 
 
 const displayController = (() => {
-    return true;
+    let gameboardDiv = document.querySelector('#gameboard');
+    const renderGameboard = () => {
+        clearCells();
+        drawCells();
+    }
+
+    const drawCells = () => {
+        let gameboardArray = Gameboard.getGameboard();
+        for (let i = 0; i < gameboardArray.length; i++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            if (!isNaN(gameboardArray[i])) {
+                cell.textContent = '-';
+            }
+            else {
+                cell.textContent = gameboardArray[i];
+            }
+            gameboardDiv.appendChild(cell);
+        }
+    }
+
+    const clearCells = () => {
+        while (gameboardDiv.firstChild) {
+            gameboardDiv.removeChild(gameboardDiv.lastChild);
+          }
+    }
+
+    return {renderGameboard};
 })();
+
 
 const Player = (name, mark) => {
     const getName = () => name;
     const getMark = () => mark;
     return {getName, getMark};
 }
+
+displayController.renderGameboard();
